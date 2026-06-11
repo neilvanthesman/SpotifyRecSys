0245668fa4640ae2e29914480921be1f62ea998f
neilvanthesman <159006778+neilvanthesman@users.noreply.github.com>
2026-06-11 21:04:29 +0700

    Update SpotifyExplore.py

--- .streamlit/SpotifyExplore.py
+++ .streamlit/SpotifyExplore.py
@@ -20,7 +20,7 @@ from google.oauth2.service_account import Credentials
 # -------------------------------------------------
 st.set_page_config(
     page_title="Spotify Song Recommender",
-    page_icon="✧",
+    page_icon="🎧",
     layout="wide"
 )
 
