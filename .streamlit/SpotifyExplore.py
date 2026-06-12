d69ec43d281edc983a1409681e7cc54a5c35eb50
neilvanthesman <159006778+neilvanthesman@users.noreply.github.com>
2026-06-12 15:15:33 +0700

    Update SpotifyExplore.py

--- .streamlit/SpotifyExplore.py
+++ .streamlit/SpotifyExplore.py
@@ -273,9 +273,9 @@ left_space, center_button, right_space = st.columns([4, 1, 4])
 with center_button:
 
     if st.button(
-        "recommend ✧ songs ✧",
+        "recommend ✧-songs-✧",
         use_container_width=True,
-        type = "secondary",
+        type = "secondary"
     ):
 
         query = f"{artist} <> {song}"
