23d2b74e38bb414cc0074c228d3bd577b2b57b05
neilvanthesman <159006778+neilvanthesman@users.noreply.github.com>
2026-06-12 13:28:37 +0700

    Update SpotifyExplore.py

--- .streamlit/SpotifyExplore.py
+++ .streamlit/SpotifyExplore.py
@@ -14,7 +14,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics.pairwise import cosine_similarity
 from google.oauth2.service_account import Credentials
 
-
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
 # -------------------------------------------------
 # Page Config
 # -------------------------------------------------
