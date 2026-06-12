984970aa514842d74fd573a5a22152b1fc00b9c1
neilvanthesman <159006778+neilvanthesman@users.noreply.github.com>
2026-06-12 15:17:01 +0700

    Update SpotifyExplore.py

--- .streamlit/SpotifyExplore.py
+++ .streamlit/SpotifyExplore.py
@@ -273,7 +273,7 @@ left_space, center_button, right_space = st.columns([4, 1, 4])
 with center_button:
 
     if st.button(
-        "recommend ✧-songs-✧",
+        "recommend⠀✧⠀songs⠀✧",
         use_container_width=True,
         type = "secondary"
     ):
